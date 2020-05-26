54b9581eef397cf8d5abfc2c30e7629a5f4108ac
dhiraj <dhirajbadu50@gmail.com>
2020-05-26 22:08:16 +0545

    ui updated

--- app.js
+++ app.js
@@ -1,10 +1,92 @@
+$('.clockpicker').clockpicker({
+	placement: 'buttom',
+	align: 'left',
+	donetext: 'Done',
+	doneclass: 'timePicked'
+});
+
+
 $(".sendRequest").click(function(){
-var url = "http://www.google.com/search?q="+currentTabUrl;
-window.open(url, '_blank');
-$("#successMsg").removeClass("hide").removeClass("show").addClass("show")
+	var time = $("#time").val();
+	var hostname = get_hostname(currentTabUrl);
+	console.log("hostname : " + hostname);
+	var isValidHostName = hostname.includes("youtube");
+	console.log("isValidHostName : " + isValidHostName);
+
+	var url = "http://localhost:8080/chromeExtention/scrap?url="+currentTabUrl+"&starttime="+time;
+	if(isValidHostName){
+		send(url);
+	}else{
+		invalidMsg();
+	}
+	
 });
 
 
 $(".closebtn").click(function(){
-$(this).parent().removeClass("hide").removeClass("show").addClass("hide")
+	$(this).parent().removeClass("hide").removeClass("show").addClass("hide")
 });
+
+function get_hostname(url) {
+	return $('<a>').prop('href', url).prop('hostname');
+
+}
+
+function send(url , param){
+	$.ajax({
+		type: "GET",
+		url: url,
+		contentType: "application/x-www-form-urlencoded;charset=utf-8",
+		dataType: 'json',
+		timeout: 30000,
+		beforeSend: function (xhr) {
+			loading("sendRequest");
+		},
+		success: function (data) {
+			unloading("sendRequest");
+			console.log("success");
+			if(data.status === 200){
+				successMsg();
+			}else{
+				errorMsg();
+			}
+			
+		},
+
+		error: function (xhr, exception) {
+			unloading("sendRequest");
+			console.log("errorMsg");
+			errorMsg();
+			
+		}
+	});
+}
+
+function successMsg(){
+	$("#successMsg").removeClass("hide").removeClass("show").addClass("show");
+	$("#errorMsg").removeClass("hide").removeClass("show").addClass("hide");
+	$("#invalidMsg").removeClass("hide").removeClass("show").addClass("hide");
+}
+
+function errorMsg(){
+	$("#successMsg").removeClass("hide").removeClass("show").addClass("hide");
+	$("#errorMsg").removeClass("hide").removeClass("show").addClass("show");
+	$("#invalidMsg").removeClass("hide").removeClass("show").addClass("hide");
+}
+
+function invalidMsg(){
+	$("#successMsg").removeClass("hide").removeClass("show").addClass("hide");
+	$("#errorMsg").removeClass("hide").removeClass("show").addClass("hide");
+	$("#invalidMsg").removeClass("hide").removeClass("show").addClass("show");
+}
+
+
+function loading(btnId){
+	$("#"+btnId ).find(".fa-refresh").removeClass("hide").removeClass("show").addClass("show");
+	$("#"+btnId).prop('disabled', true);
+}
+
+function unloading(btnId){
+	$("#"+btnId + " .fa-refresh").removeClass("hide").removeClass("show").addClass("hide");
+	$("#"+btnId).prop('disabled', false);
+}
--- background.js
+++ background.js
@@ -2,10 +2,12 @@ console.log("loaded");
 
 
 var currentTabUrl = "";
+var hostname = "";
 
 
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	 currentTabUrl = tabs[0].url
+	 hostname = tabs[0].hostname
 	  console.log(currentTabUrl);
 
 })
